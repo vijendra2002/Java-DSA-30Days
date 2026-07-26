7fd9a05adbd8fb7a1eb23207158704122f4a42a3
vijendra2002 <vijendrasinghparmar553@gmail.com>
2026-07-26 23:00:36 +0530

    Add Day 05 Java pattern programs

---
+++ src/Day05_Patterns/ButterflyPattern.java
@@ -0,0 +1,51 @@
+package Day05_Patterns;
+
+public class ButterflyPattern {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+
+            // Left Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n; i >= 1; i--) {
+
+            // Left Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ src/Day05_Patterns/FloydsTriangle.java
@@ -0,0 +1,22 @@
+package Day05_Patterns;
+
+/**
+ * FloydsTriangle
+ */
+public class FloydsTriangle {
+
+  public static void main(String[] args) {
+    int n = 5;
+    int num = 1;
+
+    // outer loop //
+    for(int i=1; i<=n; i++){
+      // inner loop//
+      for(int j=1; j<=i; j++){
+        System.out.print(num);
+        num++;
+      }
+      System.out.println();
+    }
+  }
+}
